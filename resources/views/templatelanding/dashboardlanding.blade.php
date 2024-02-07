ee27d1947d2260ddb2893718247f0863e6b80e01
yusup159 <101506490+yusup159@users.noreply.github.com>
2024-02-07 13:30:49 +0700

    fixed beranda

--- resources/views/templatelanding/dashboardlanding.blade.php
+++ resources/views/templatelanding/dashboardlanding.blade.php
@@ -55,7 +55,8 @@
                     <h1>
                         Selamat Datang Di Website Pondok Pesantren Thoriqul Irsyad
                     </h1>
-                    <p>Sebuah lembaga pendidikan modern yang mendorong kreativitas mahasiswa untuk berkontribusi pada kemajuan bangsa.</p>
+                    <p>Sebuah lembaga pendidikan modern yang mendorong kreativitas mahasiswa untuk berkontribusi pada
+                        kemajuan bangsa.</p>
                     <a href="{{ route('profil') }}">
                         <button class="btn-ppdb">
                             <img src="{{ asset('pesantren/asset/icon/arrow.svg') }}" alt="">Lihat Profil &
@@ -64,7 +65,7 @@
                     </a>
                 </div>
                 <div class="col-lg-5 hero-kanan mt-6">
-                    <img src="{{ asset('pesantren/asset/img/heroKanan.jpg') }}" alt="">
+                    <img src="{{ asset('pesantren/asset/img/selamatdatang.jpg') }}" alt="">
                 </div>
             </div>
         </div>
@@ -76,70 +77,23 @@
             <div class="header text-center">
                 <h3>Berpengalaman & Berdedikasi tinggi</h3>
                 <h1> Pengasuh dan Pengurus<br>Pondok Pesantren</h1>
-            </div>
-            <!-- Slider Code -->
-            <div class="slider-container">
-                <div class="swiper">
-                    <!-- Additional required wrapper -->
-                    <div class="swiper-wrapper">
-                        <!-- Slides -->
-                        <div class="swiper-slide">
-                            <div class="card rounded">
-                                <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
-                                    <div class="image">
-                                        <img src="{{ asset('pesantren/asset/img/download.jpeg') }}" alt="User Image"
-                                            class="img-circle elevation-2"
-                                            style="width: 100px; height: 100px; object-fit: cover;">
-                                    </div>
-                                    <div class="info ml-3">
-                                        <h6 class="d-block">yuo</h6>
-
-                                    </div>
-
+            </div> 
+                        <div class="slider-container">
+                            <div class="slider-wrapper">
+                                <div class="slide">
+                                    <img src="{{ asset('pesantren/asset/img/usttads/Adi.svg') }}" alt="Slide 3">
                                 </div>
-                                <p>dfjdb</p>
+                                <div class="slide">
+                                    <img src="{{ asset('pesantren/asset/img/usttads/Felix.svg') }}" alt="Slide 1">
+                                </div>
+                                <div class="slide">
+                                    <img src="{{ asset('pesantren/asset/img/usttads/Gus Iqdam.svg') }}" alt="Slide 2">
+                                </div>
+                                <div class="slide">
+                                    <img src="{{ asset('pesantren/asset/img/usttads/Hanan Ataki.svg') }}" alt="Slide 3">
+                                </div>
                             </div>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="card rounded">
-                                <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
-                                    <div class="image">
-                                        <img src="{{ asset('pesantren/asset/img/download.jpeg') }}" alt="User Image"
-                                            class="img-circle elevation-2"
-                                            style="width: 100px; height: 100px; object-fit: cover;">
-                                    </div>
-                                    <div class="info ml-3">
-                                        <h6 class="d-block">yuo</h6>
-
-                                    </div>
-
-                                </div>
-                                <p>dfjdb</p>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="card rounded">
-                                <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
-                                    <div class="image">
-                                        <img src="{{ asset('pesantren/asset/img/download.jpeg') }}" alt="User Image"
-                                            class="img-circle elevation-2"
-                                            style="width: 100px; height: 100px; object-fit: cover;">
-                                    </div>
-                                    <div class="info ml-3">
-                                        <h6 class="d-block">yuo</h6>
-
-                                    </div>
-
-                                </div>
-                                <p>dfjdb</p>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-
         </div>
     </div>
     <!-- End Partnership -->
@@ -150,27 +104,26 @@
             <h3>Pusat Informasi</h3>
             <h1>Informasi Terbaru Pondok Pesantren</h1>
         </div>
-        <div class="bungkus-kegiatan">
+        <div class="bungkus-galang">
             <div class="row">
-                @foreach($kegiatan as $key => $kgtn)
-                <div class="card col-lg-4 mb-3">
-                    <a href="{{ route('detailkegiatan') }}">
-                        <img src="{{ asset('storage/fotokegiatan/' . basename($kgtn->fotokegiatan)) }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $kgtn->judul }}</h5>
-                            <?php
-                                $deskripsi = $kgtn->deskripsi;
-                                $potongan_deskripsi = substr($deskripsi, 0, 250); 
-                                echo "<p class='text-truncate'>$potongan_deskripsi...</p>";
-                                ?>
-                        </div>
-                    </a>
-                </div>
+                @foreach ($kegiatan as $key => $kegi)
+                    <div class="card col-lg-4 mb-3">
+                        <a href="{{ route('detailberita') }}">
+                            <img src="{{ asset('storage/fotokegiatan/' . basename($kegi->fotokegiatan)) }}"
+                                class="card-img-top" style="height: 200px;" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $kegi->judul }}</h5>
+                                <p>{!! substr(strip_tags($kegi->deskripsi), 0, 100) !!}...</p>
+                            </div>
+                        </a>
+                    </div>
                 @endforeach
             </div>
         </div>
+        
+        
     </div>
-    
+
     <!-- Card -->
 
     <!-- Review -->
@@ -187,7 +140,7 @@
 
                 </div>
                 <div class="col-lg-5 hero-kanan-review" style="margin-top: 30px; margin-bottom: 30px;">
-                    <img src="{{ asset('pesantren/asset/img/27da5d0dc1a280ab5b29b1429b8a68f6.jpeg') }}"
+                    <img src="{{ asset('pesantren/asset/img/generasi.jpg') }}"
                         style="width: 600px; height: 420px;">
                 </div>
             </div>
@@ -202,11 +155,10 @@
                 <!-- Column 1 -->
                 <div class="col-md-4">
                     <h5>Menu</h5>
-                    <p><a class="link-opacity-100" href="./index.html">Beranda</a></p>
-                    <p><a class="link-opacity-100" href="./profil.html">Profil</a></p>
-                    <p><a class="link-opacity-100" href="./news.html">Portal Berita</a></p>
-                    <p><a class="link-opacity-100" href="./faq.html">FAQ</a></p>
-
+                    <p><a class="link-opacity-100" href="{{ route('index') }}">Beranda</a></p>
+                    <p><a class="link-opacity-100" href="{{ route('profil') }}">Profil</a></p>
+                    <p><a class="link-opacity-100" href="{{ route('berita') }}">Portal Berita</a></p>
+                    <p><a class="link-opacity-100" href="https://wa.me/6285293239446">FAQ</a></p>
                 </div>
 
                 <!-- Column 2 -->
@@ -220,7 +172,7 @@
                 <!-- Column 3 -->
                 <div class="col-md-4">
                     <h5>PondokPesantren</h5>
-                    <p>0272 - 897237</p>
+                    <p>+62 852-9323-9446</p>
                     <p>PondokPesantren.com</p>
                 </div>
             </div>
@@ -231,34 +183,7 @@
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script type="module">
-        import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-
-        const swiper = new Swiper('.swiper', {
-            direction: 'horizontal',
-            loop: true,
-            autoplay: {
-                delay: 2000,
-            },
-            effect: 'coverflow', // Ganti efek menjadi coverflow
-            grabCursor: true,
-            centeredSlides: true,
-            slidesPerView: 'auto',
-            coverflowEffect: {
-                rotate: 50, // Sudut rotasi kartu
-                stretch: 0, // Jarak antar kartu
-                depth: 100, // Kedalaman kartu
-                modifier: 1, // Modifikasi efek
-            },
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        });
-    </script>
+    
 </body>
 
 </html>
